208f91c947c955978bff51a3af2219a1e4f0ac78
Lizardin1 <gustavolizardo123oliveira@gmail.com>
2025-03-12 22:28:26 -0300

    #

--- src/pages/termos-privacidade/Termos.jsx
+++ src/pages/termos-privacidade/Termos.jsx
@@ -15,291 +15,68 @@ export default function Termos() {
           </Link>
           <div className="section-termos">
             <div className="section-termos-content">
-              <h1>POLÍTICA DE PRIVACIDADE - Matheus</h1>
-
-              <div class="animation">
-                <h2>1. Introdução</h2>
-                <p>
-                  A Matheus, pessoa jurídica de direito privado, com sede na Rua
-                  Mato Grosso, inscrita no CNPJ/MF sob o nº 52.878.843/0001-99
-                  (“Lojista” ou “nós”), leva a sua privacidade a sério e zela
-                  pela segurança e proteção de dados de todos os seus clientes,
-                  parceiros, fornecedores e usuários (“Usuários” ou “você”) do
-                  site{" "}
-                  <a href="https://www.lojadecorecasa.com.br/">
-                    https://www.lojadecorecasa.com.br/
-                  </a>{" "}
-                  e qualquer outro site, loja ou aplicativo operado pelo Lojista
-                  (aqui designados, simplesmente, “Loja”).
-                </p>
-                <p>
-                  Esta Política de Privacidade (“Política de Privacidade”)
-                  destina-se a informá-lo sobre o modo como nós utilizamos e
-                  divulgamos informações coletadas em suas visitas à nossa Loja
-                  e em mensagens que trocamos com você (“Comunicações”).
-                </p>
-                <p>
-                  AO ACESSAR A LOJA, ENVIAR COMUNICAÇÕES OU FORNECER QUALQUER
-                  TIPO DE DADO PESSOAL, VOCÊ DECLARA ESTAR CIENTE E DE ACORDO
-                  COM ESTA POLÍTICA DE PRIVACIDADE, A QUAL DESCREVE AS
-                  FINALIDADES E FORMAS DE TRATAMENTO DE SEUS DADOS PESSOAIS QUE
-                  VOCÊ DISPONIBILIZAR NA LOJA.
-                </p>
-              </div>
+              <h1>Termos de Serviço</h1>
+              <h2>Termos de Serviço da 52.878.843 MATHEUS RODRIGUES (52.878.843 MATHEUS RODRIGUES)
+              </h2> <p><strong>Última Atualização:</strong> [05/03/2024]</p>
               <br />
-              <div class="animation">
-                <h2>2. Escopo e Aplicação</h2>
-                <p>
-                  Esta Política de Privacidade fornece uma visão geral de nossas
-                  práticas de privacidade e das escolhas que você pode fazer,
-                  bem como direitos que você pode exercer em relação aos Dados
-                  Pessoais tratados por nós. Se você tiver alguma dúvida sobre o
-                  uso de Dados Pessoais, entre em contato com{" "}
-                  <a href="mailto:matheusr2015@gmail.com">
-                    matheusr2015@gmail.com
-                  </a>
-                  .
-                </p>
-                <p>
-                  Além disso, a Política de Privacidade não se aplica a
-                  quaisquer aplicativos, produtos, serviços, site ou recursos de
-                  mídia social de terceiros que possam ser oferecidos ou
-                  acessados por meio da Loja. O acesso a esses links fará com
-                  que você deixe a Loja e possa resultar na coleta ou
-                  compartilhamento de informações sobre você por terceiros.
-                </p>
-                <p>
-                  Nós não controlamos, endossamos ou fazemos quaisquer
-                  representações sobre esses sites de terceiros ou suas práticas
-                  de privacidade, que podem ser diferentes das nossas.
-                  Recomendamos que você revise a política de privacidade de
-                  qualquer site com o qual você interaja antes de permitir a
-                  coleta e o uso de seus Dados Pessoais.
-                </p>
-              </div>
+              <p>
+                Bem-vindo(a) à 52.878.843 MATHEUS RODRIGUES Estes Termos de Serviço (“Termos”) regem seu acesso e uso dos nossos aplicativos e serviços disponibilizados através do Facebook Developer (“Serviços”). Ao acessar ou usar os Serviços, você concorda em estar vinculado(a) por estes Termos.
+              </p>
               <br />
-              <div class="animation">
-                <h2>3. Definições</h2>
-                <p>Para os fins desta Política de Privacidade:</p>
-                <ul>
-                  <li>
-                    <strong>“Dados Pessoais”</strong>: significa qualquer
-                    informação que, direta ou indiretamente, identifique ou
-                    possa identificar uma pessoa natural, como por exemplo,
-                    nome, CPF, data de nascimento, endereço IP, dentre outros.
-                  </li>
-                  <li>
-                    <strong>“Dados Pessoais Sensíveis”</strong>: significa
-                    qualquer informação que revele, em relação a uma pessoa
-                    natural, origem racial ou étnica, convicção religiosa,
-                    opinião política, filiação a sindicato ou a organização de
-                    caráter religioso, filosófico ou político, dado referente à
-                    saúde ou à vida sexual, dado genético ou biométrico.
-                  </li>
-                  <li>
-                    <strong>“Tratamento de Dados Pessoais”</strong>: significa
-                    qualquer operação efetuada no âmbito dos Dados Pessoais, por
-                    meio de meios automáticos ou não, tal como a recolha,
-                    gravação, organização, estruturação, armazenamento,
-                    adaptação ou alteração, recuperação, consulta, utilização,
-                    divulgação por transmissão, disseminação ou,
-                    alternativamente, disponibilização, harmonização ou
-                    associação, restrição, eliminação ou destruição. Também é
-                    considerado Tratamento de Dados Pessoais qualquer outra
-                    operação prevista nos termos da legislação aplicável.
-                  </li>
-                  <li>
-                    <strong>“Leis de Proteção de Dados”</strong>: significa
-                    todas as disposições legais que regulem o Tratamento de
-                    Dados Pessoais, incluindo, porém sem se limitar, a Lei nº
-                    13.709/18, Lei Geral de Proteção de Dados Pessoais (“LGPD”).
-                  </li>
-                </ul>
-              </div>
+              <h3>1. Uso dos Serviços</h3>
+              <p>
+                Você pode usar os Serviços apenas de acordo com estes Termos e todas as leis, regras e regulamentos aplicáveis. Ao usar os Serviços, você concorda que:
+              </p>
+              <ul>
+                <li>Não irá utilizar os Serviços para qualquer propósito ilegal ou não autorizado;</li>
+                <li>Não irá acessar ou tentar acessar os Serviços por quaisquer meios que não sejam através das interfaces fornecidas pela 52.878.843 MATHEUS RODRIGUES;</li>
+                <li>Não irá usar os Serviços de maneira que possa interromper, danificar ou afetar negativamente o acesso de outros usuários aos Serviços.</li>
+              </ul>
               <br />
-              <div class="animation">
-                <h2>4. Uso de Dados Pessoais</h2>
-                <p>
-                  Coletamos e usamos Dados Pessoais para gerenciar seu
-                  relacionamento conosco e melhor atendê-lo quando você estiver
-                  adquirindo produtos e/ou serviços na Loja, personalizando e
-                  melhorando sua experiência. Exemplos de como usamos os dados
-                  incluem:
-                </p>
-                <ul>
-                  <li>
-                    Viabilizar que você adquira produtos e/ou serviços na Loja;
-                  </li>
-                  <li>
-                    Para confirmar ou corrigir as informações que temos sobre
-                    você;
-                  </li>
-                  <li>
-                    Para enviar informações que acreditamos ser do seu
-                    interesse;
-                  </li>
-                  <li>Para personalizar sua experiência de uso da Loja;</li>
-                  <li>
-                    Para personalizar o envio de publicidades para você, baseada
-                    em seu interesse em nossa Loja;
-                  </li>
-                  <li>
-                    Para entrarmos em contato por um número de telefone e/ou
-                    endereço de e-mail fornecido.
-                  </li>
-                </ul>
-                <p>
-                  Podemos entrar em contato com você pessoalmente, por mensagem
-                  de voz, através de equipamentos de discagem automática, por
-                  mensagens de texto (SMS), por e-mail, ou por qualquer outro
-                  meio de comunicação que seu dispositivo seja capaz de receber,
-                  nos termos da lei e para fins comerciais razoáveis.
-                </p>
-              </div>
+              <h3>2. Conteúdo do Usuário</h3>
+              <p>
+                Você pode postar, linkar, armazenar ou compartilhar conteúdo nos Serviços. Você é responsável pelo conteúdo que disponibiliza, incluindo sua legalidade, confiabilidade e adequação. Ao postar conteúdo nos Serviços, você concede à 52.878.843 MATHEUS RODRIGUES uma licença mundial, não exclusiva, livre de royalties, com o direito de sublicenciar, usar, copiar, modificar, criar obras derivadas, distribuir, publicar e processar esse conteúdo sem qualquer compensação adicional para você.
+              </p>
               <br />
-              <div class="animation">
-                <h2>5. Compartilhamento de Dados Pessoais</h2>
-                <p>Nós poderemos compartilhar seus Dados Pessoais:</p>
-                <ul>
-                  <li>
-                    Com a(s) empresa(s) parceira(s) que você selecionar ou optar
-                    em enviar os seus dados, dúvidas, perguntas etc.;
-                  </li>
-                  <li>
-                    Com provedores de serviços ou parceiros para gerenciar ou
-                    suportar certos aspectos de nossas operações comerciais em
-                    nosso nome;
-                  </li>
-                  <li>
-                    Com terceiros, com o objetivo de nos ajudar a gerenciar a
-                    Loja;
-                  </li>
-                  <li>
-                    Com terceiros, caso ocorra qualquer reorganização, fusão,
-                    venda, joint venture, cessão, transmissão ou transferência
-                    de toda ou parte da nossa empresa, ativo ou capital
-                    (incluindo os relativos à falência ou processos
-                    semelhantes).
-                  </li>
-                </ul>
-              </div>
+              <h3>3. Contas de Usuário</h3>
+              <p>
+                Você pode precisar de uma conta para usar alguns dos nossos Serviços. Você é responsável por manter a confidencialidade da sua conta e senha e por restringir o acesso ao seu computador ou dispositivo. Você concorda em aceitar responsabilidade por todas as atividades que ocorram sob sua conta ou senha.
+              </p>
               <br />
-              <div class="animation">
-                <h2>6. Direitos do Usuário</h2>
-                <p>Você pode, a qualquer momento, requerer:</p>
-                <ul>
-                  <li>
-                    Confirmação de que seus Dados Pessoais estão sendo tratados;
-                  </li>
-                  <li>Acesso aos seus Dados Pessoais;</li>
-                  <li>
-                    Correções a dados incompletos, inexatos ou desatualizados;
-                  </li>
-                  <li>
-                    Anonimização, bloqueio ou eliminação de dados
-                    desnecessários, excessivos ou tratados em desconformidade
-                    com o disposto em lei;
-                  </li>
-                  <li>
-                    Portabilidade de Dados Pessoais a outro prestador de
-                    serviços, contanto que isso não afete nossos segredos
-                    industriais e comerciais;
-                  </li>
-                  <li>
-                    Eliminação de Dados Pessoais tratados com seu consentimento,
-                    na medida do permitido em lei;
-                  </li>
-                  <li>
-                    Informações sobre as entidades às quais seus Dados Pessoais
-                    tenham sido compartilhados;
-                  </li>
-                  <li>
-                    Informações sobre a possibilidade de não fornecer o
-                    consentimento e sobre as consequências da negativa;
-                  </li>
-                  <li>Revogação do consentimento.</li>
-                </ul>
-              </div>
+              <h3>4. Direitos de Propriedade Intelectual</h3>
+              <p>
+                Os Serviços e todo o material neles contido (exceto o conteúdo fornecido por usuários) são propriedade da 52.878.843 MATHEUS RODRIGUES ou de seus licenciadores e são protegidos por direitos autorais, marcas registradas e outras leis de propriedade intelectual.
+              </p>
               <br />
-              <div class="animation">
-                <h2>7. Segurança dos Dados Pessoais</h2>
-                <p>
-                  Buscamos adotar as medidas técnicas e organizacionais
-                  previstas pelas Leis de Proteção de Dados adequadas para
-                  proteção dos Dados Pessoais na nossa organização.
-                  Infelizmente, nenhuma transmissão ou sistema de armazenamento
-                  de dados tem a garantia de serem 100% seguros.
-                </p>
-                <p>
-                  Caso tenha motivos para acreditar que sua interação conosco
-                  tenha deixado de ser segura (por exemplo, caso acredite que a
-                  segurança de qualquer uma de suas contas foi comprometida),
-                  favor nos notificar imediatamente.
-                </p>
-              </div>
+              <h3>5. Modificações e Interrupção dos Serviços</h3>
+              <p>
+                Reservamo-nos o direito de modificar ou descontinuar, temporária ou permanentemente, os Serviços (ou qualquer parte deles) com ou sem aviso prévio. Não seremos responsáveis perante você ou qualquer terceiro por qualquer modificação, suspensão ou descontinuação dos Serviços.
+              </p>
               <br />
-              <div class="animation">
-                <h2>8. Atualizações desta Política de Privacidade</h2>
-                <p>
-                  Se modificarmos nossa Política de Privacidade, publicaremos o
-                  novo texto na Loja, com a data de revisão atualizada. Podemos
-                  alterar esta Política de Privacidade a qualquer momento. Caso
-                  haja alteração significativa nos termos dessa Política de
-                  Privacidade, podemos informá-lo por meio das informações de
-                  contato que tivermos em nosso banco de dados ou por meio de
-                  notificação em nossa Loja.
-                </p>
-                <p>
-                  Recordamos que nós temos como compromisso não tratar os seus
-                  Dados Pessoais de forma incompatível com os objetivos
-                  descritos acima, exceto se de outra forma requerido por lei ou
-                  ordem judicial.
-                </p>
-                <p>
-                  Sua utilização da Loja após as alterações significa que
-                  aceitou as Políticas de Privacidade revisadas. Caso, após a
-                  leitura da versão revisada, você não esteja de acordo com seus
-                  termos, favor encerrar o acesso à Loja.
-                </p>
-              </div>
+              <h3>6. Isenções de Responsabilidade e Limitações de Responsabilidade</h3>
+              <p>
+                Os Serviços são fornecidos "como estão" e "conforme disponíveis". Não garantimos que os Serviços serão ininterruptos ou livres de erros. Em nenhum caso a 52.878.843 MATHEUS RODRIGUES será responsável por quaisquer danos diretos, indiretos, incidentais, especiais ou consequenciais decorrentes do uso ou incapacidade de usar os Serviços.
+              </p>
               <br />
-              <div class="animation">
-                <h2>9. Contato</h2>
-                <p>
-                  Caso pretenda exercer qualquer um dos direitos previstos nesta
-                  Política de Privacidade e/ou nas Leis de Proteção de Dados, ou
-                  resolver quaisquer dúvidas relacionadas ao Tratamento de seus
-                  Dados Pessoais, favor contatar-nos através do e-mail{" "}
-                  <a href="mailto:matheusr2015@gmail.com">
-                    matheusr2015@gmail.com
-                  </a>
-                  .
-                </p>
-              </div>
+              <h3>7. Indenização</h3>
+              <p>
+                Você concorda em indenizar e isentar a 52.878.843 MATHEUS RODRIGUES, seus diretores, funcionários e agentes, de e contra quaisquer reclamações, danos, obrigações, perdas, responsabilidades, custos ou dívidas e despesas decorrentes de:
+              </p>
+              <ul>
+                <li>Seu uso e acesso aos Serviços;</li>
+                <li>Sua violação de qualquer termo destes Termos.</li>
+              </ul>
+              <br />
+              <h3>8. Alterações aos Termos</h3>
+              <p>
+                Podemos modificar estes Termos de tempos em tempos. Se fizermos alterações materiais, forneceremos a você um aviso através dos Serviços ou por outros meios, para oferecer a oportunidade de revisar as alterações antes que elas entrem em vigor. Seu uso contínuo dos Serviços após a publicação ou envio de um aviso sobre nossas alterações aos Termos significa que você concorda com os Termos atualizados.
+              </p>
+              <br />
+              <h3>9. Contato</h3>
+              <p>
+                Se você tiver qualquer dúvida sobre estes Termos, por favor, entre em contato conosco pelo e-mail: <a href="mailto:matheusdsng@gmail.com">matheusdsng@gmail.com</a>. Ao acessar ou usar nossos Serviços, você concorda em estar vinculado(a) por estes Termos.
+              </p>
             </div>
-          </div>
-      <Link to="/">
-        <div className="return">
-          <BiArrowBack className="icon-back" />
-        </div>
-      </Link>
-          <div className="footer">
-            <div className="title-footer">
-              <div>
-                <h2>
-                  <a href="">Termos de uso</a>
-                </h2>
-              </div>
-              <div>
-                <h2>
-                  <a href="">Políticas de privacidade</a>
-                </h2>
-              </div>
-            </div>
-            <p>
-              ©2025 – Todos os direitos reservados | CNPJ: 52.878.843/0001-99 |
-              Matheus Rodrigues
-            </p>
           </div>
         </div>
       </div>
--- src/pages/termos-privacidade/politicas.jsx
+++ src/pages/termos-privacidade/politicas.jsx
@@ -1,9 +1,116 @@
-export default function Politicas(){
+import './politicas.css'
+import { Link } from 'react-router-dom'
+import { BiArrowBack } from 'react-icons/bi'
+import Header from '../components/Header'
+export default function Politicas() {
 
     return (
-
         <>
-            <h1 style={{color: "black", fontSize: "50px"}}>Políticas</h1>
+            <div className="container">
+                <div className="background">
+                    <Header />
+                    <Link to="/">
+                        <div className="return">
+                            <BiArrowBack className="icon-back" />
+                        </div>
+                    </Link>
+                    <div className="section-termos">
+                        <div className="section-termos-content">
+                            <h1>Política de Privacidade</h1>
+                            <h2>Política de Privacidade da 52.878.843 MATHEUS RODRIGUES(52.878.843 MATHEUS RODRIGUES)
+                            </h2> <p><strong>Última Atualização:</strong> [05/03/2024]</p>
+                            <br />
+                            <p>
+                                A Matheus (52.878.843 MATHEUS RODRIGUES), doravante referida como “52.878.843 MATHEUS RODRIGUES”, “nós” ou “nosso(a)”, está comprometida em proteger a privacidade e a segurança das informações pessoais dos usuários de nossos aplicativos disponibilizados através do Facebook Developer. Esta Política de Privacidade (“Política”) explica como coletamos, usamos, compartilhamos e protegemos suas informações pessoais ao usar nossos serviços (“Serviços”).
+                            </p>
+                            <br />
+                            <h3>1. Informações que Coletamos</h3>
+                            <p>
+                                Coletamos informações que você nos fornece diretamente, informações geradas automaticamente ao usar nossos Serviços, e informações de terceiros, incluindo o Facebook. Isso pode incluir:
+                            </p>
+                            <ul>
+                                <li><strong>Informações fornecidas pelo usuário:</strong> Inclui dados pessoais fornecidos ao criar uma conta, como nome, e-mail, número de telefone etc.</li>
+                                <li><strong>Informações geradas automaticamente:</strong> Inclui informações sobre como você usa nossos Serviços, como registros de acesso, dados de localização, e interações com os Serviços.</li>
+                                <li><strong>Informações de terceiros:</strong> Recebemos informações sobre você de outras fontes, incluindo o Facebook, como quando você acessa nossos Serviços através de uma conta do Facebook.</li>
+                            </ul>
+                            <br />
+                            <h3>2. Uso das Informações</h3>
+                            <p>
+                                Usamos as informações coletadas para fornecer, manter, melhorar e promover nossos Serviços, incluindo para:
+                            </p>
+                            <ul>
+                                <li>Criar e gerenciar contas de usuário;</li>
+                                <li>Oferecer suporte ao cliente e responder a solicitações e perguntas;</li>
+                                <li>Melhorar a segurança e a proteção dos nossos Serviços;</li>
+                                <li>Realizar análises e pesquisas internas para melhorar e desenvolver novos produtos e serviços;</li>
+                                <li>Enviar comunicações de marketing, sujeito às suas preferências e à legislação aplicável.</li>
+                            </ul>
+                            <p>
+                                <strong>Uso dos Dados da Plataforma:</strong>
+                            </p>
+                            <ul>
+                                <li>Explicar o Uso dos Dados: Detalhamos como usamos os dados de maneira ética e em conformidade com as políticas de privacidade.</li>
+                                <li>Focar em Dados Públicos e Estatísticos: Acessamos apenas dados públicos para fins estatísticos e analíticos, sem a intenção de coletar dados pessoais privados.</li>
+                                <li>Garantir Segurança e Conformidade: Implementamos medidas de segurança adequadas para proteger os dados e operamos em conformidade com as leis de proteção de dados aplicáveis.</li>
+                            </ul>
+                            <br />
+                            <h3>3. Compartilhamento de Informações</h3>
+                            <p>
+                                Podemos compartilhar suas informações pessoais nas seguintes circunstâncias:
+                            </p>
+                            <ul>
+                                <li>Com fornecedores, consultores e outros prestadores de serviços que precisam acessar tais informações para realizar trabalhos em nosso nome;</li>
+                                <li>Em resposta a um pedido legal ou para proteger nossos direitos, propriedade ou segurança, ou os direitos, propriedade ou segurança de terceiros;</li>
+                                <li>Em conexão com, ou durante negociações de, qualquer fusão, venda de ativos da empresa, financiamento ou aquisição de todo ou parte de nosso negócio por outra empresa;</li>
+                                <li>Entre nossas empresas afiliadas, em conformidade com esta Política.</li>
+                            </ul>
+                            <br />
+                            <h3>4. Segurança das Informações</h3>
+                            <p>
+                                Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado, alteração, divulgação ou destruição. Contudo, nenhum sistema de segurança é infalível, e não podemos garantir a segurança das suas informações.
+                            </p>
+                            <br />
+                            <h3>5. Seus Direitos</h3>
+                            <p>
+                                Reservamo-nos o direito de modificar ou descontinuar, temporária ou permanentemente, os Serviços (ou qualquer parte deles) com ou sem aviso prévio. Não seremos responsáveis perante você ou qualquer terceiro por qualquer modificação, suspensão ou descontinuação dos Serviços.
+                            </p>
+                            <br />
+                            <h3>6. Alterações na Política de Privacidade</h3>
+                            <p>
+                                Podemos atualizar esta Política de tempos em tempos. Quando fizermos alterações, atualizaremos a data de "última atualização" no início da Política e postaremos a nova Política.
+                            </p>
+                            <br />
+                            <h3>7. Contato</h3>
+                            <p>
+                                Se tiver dúvidas sobre esta Política de Privacidade, entre em contato conosco pelo e-mail: <a href="mailto:matheusdsng@gmail.com">contato@bracaiolitech.com</a>. Ao utilizar nossos Serviços, você confirma que leu e compreendeu esta Política de Privacidade.
+                            </p>
+                        </div>
+                    </div>
+                    <Link to="/">
+                        <div className="return">
+                            <BiArrowBack className="icon-back" />
+                        </div>
+                    </Link>
+                    <div className="footer">
+                        <div className="title-footer">
+                            <div>
+                                <h2>
+                                    <a href="">Termos de uso</a>
+                                </h2>
+                            </div>
+                            <div>
+                                <h2>
+                                    <a href="">Políticas de privacidade</a>
+                                </h2>
+                            </div>
+                        </div>
+                        <p>
+                            ©2025 – Todos os direitos reservados | CNPJ: 52.878.843/0001-99 |
+                            Matheus Rodrigues
+                        </p>
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
